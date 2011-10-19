87b97be87350fc50744692890c47895f8a35a400
git_name <git_email>
2011-10-19 13:20:35 -0400

    Pushing new changes

---
+++ source/app.specs/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace app.specs
+{
+    public class Calculator
+    {
+        public static int when_adding_two_numbers(int i, int i1)
+        {
+            return i + i1;
+        }
+    }
+}
---
+++ source/app.specs/CalculatorSpecs.cs
@@ -0,0 +1,14 @@
+﻿using Machine.Specifications;
+
+namespace app.specs
+{
+    public class CalculatorSpecs
+    {
+        public class when_adding_two_numbers
+        {
+            private It should_return_the_sum = () => 
+                Calculator.when_adding_two_numbers(2,3).ShouldEqual(5);
+            private static int result;
+        } 
+    }
+}

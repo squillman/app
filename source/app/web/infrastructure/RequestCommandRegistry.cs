02509e933b25564a20a1b5eeb70e5b0491d61ebf
git_name <git_email>
2011-10-19 16:55:08 -0400

    Pushing new changes

--- source/app/web/infrastructure/RequestCommandRegistry.cs
+++ source/app/web/infrastructure/RequestCommandRegistry.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace app.web.infrastructure
 {
-    public class CommandRegistry : IFindCommandsThatCanProcessRequests
+    public class RequestCommandRegistry : IFindCommandsThatCanProcessRequests
     {
         public IProcessOneSpecificTypeOfRequest get_the_command_that_can_process(IContainRequestDetails request)
         {

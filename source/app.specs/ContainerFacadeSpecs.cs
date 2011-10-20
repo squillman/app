d5f4e587ee5fb4c2198272a3bc88dc5c5c3f2ccd
Ralph Bohnet <rbohnet@gmail.com>
2011-10-20 16:48:08 -0600

    Pushing new changes

---
+++ source/app.specs/ContainerFacadeSpecs.cs
@@ -0,0 +1,42 @@
+﻿using Machine.Specifications;
+using app.infrastructure.containers;
+using developwithpassion.specifications.rhinomocks;
+
+namespace app.specs
+{
+    [Subject(typeof (ContainerFacade))]
+    public class ContainerFacadeSpecs
+    {
+        public abstract class concern : Observes<IFetchDependencies, ContainerFacade>
+        {
+    }
+
+        public class when_fetching_a_dependent_instance : concern
+        {
+            Establish c = () =>
+            {
+               
+  
+            };
+
+            Because b = () => result = sut.an<AClassWithDependencies>();
+
+            It should_return_an_instance_of_the_class_with_all_its_dependencies = () =>
+                result.ShouldBeTheSameAs(the_class_with_dependencies);
+
+
+            static AClassWithDependencies result;
+            static AClassWithDependencies the_class_with_dependencies;
+        }
+    }
+
+    public class AClassWithDependencies
+    {
+        DependsOnA dependsOnA { get; set; }
+    }
+
+    public class DependsOnA
+    {
+        
+    }
+}
---
+++ source/app/infrastructure/containers/ContainerFacade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace app.infrastructure.containers 
+{
+    public class ContainerFacade : IFetchDependencies
+    {
+        public Dependency an<Dependency>()
+        {
+            // create a fully populated new instance
+            return null;
+        }
+    }
+
+}

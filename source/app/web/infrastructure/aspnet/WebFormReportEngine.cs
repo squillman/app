c053acfd3504a097be6ee54ce5a856b4ef30d435
git_name <git_email>
2011-10-20 14:11:56 -0400

    Pushing new changes

--- source/app/web/infrastructure/aspnet/WebFormReportEngine.cs
+++ source/app/web/infrastructure/aspnet/WebFormReportEngine.cs
@@ -1,17 +1,21 @@
-﻿namespace app.web.infrastructure.aspnet
+﻿using System.Web;
+
+namespace app.web.infrastructure.aspnet
 {
     public class WebFormReportEngine : IDisplayInformation
     {
         private ICreateTemplateInstances view_factory;
+        private HttpContext current_context;
 
-        public WebFormReportEngine(ICreateTemplateInstances viewFactory)
+        public WebFormReportEngine(ICreateTemplateInstances viewFactory, HttpContext currentContext)
         {
             this.view_factory = viewFactory;
+            current_context = currentContext;
         }
 
         public void display<ReportModel>(ReportModel item_to_display)
         {
-            view_factory.create_view_to_display(item_to_display);
+            view_factory.create_view_to_display(item_to_display).ProcessRequest(current_context);
         }
     }
 }

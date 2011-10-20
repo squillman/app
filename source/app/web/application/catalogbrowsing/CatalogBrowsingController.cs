6d44a201890a75ee0972e34aca0f9f1548a3c147
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-10-20 13:54:34 -0600

    Updated

---
+++ source/app/web/application/catalogbrowsing/CatalogBrowsingController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using app.models;
+using app.tasks;
+
+namespace app.web.application.catalogbrowsing
+{
+    public class CatalogBrowsingController
+    {
+        IProvideInformationAboutTheStore store;
+
+        public CatalogBrowsingController(IProvideInformationAboutTheStore store)
+        {
+            this.store = store;
+        }
+
+        public IEnumerable<DepartmentItem> get_the_main_departments()
+        {
+            return store.get_the_main_departments();
+        }
+
+        public IEnumerable<DepartmentItem> get_all_the_departments_in(ViewDepartmentsRequest parent_department)
+        {
+            return store.get_all_the_departments_in(parent_department);
+        }
+
+        public IEnumerable<ProductItem> get_all_the_products_in(ViewProductsRequest request)
+        {
+            return store.get_all_the_products_in(request);
+        }
+    }
+}

1170a4a49315b8b7674bd7d9de55e1dfd00c3538
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-10-21 17:38:05 -0600

    Updated

--- source/app/infrastructure/DelegateExtensions.cs
+++ source/app/infrastructure/DelegateExtensions.cs
@@ -6,7 +6,16 @@ namespace app.infrastructure
     {
         public static Func<ResultToCache> memoize<ResultToCache>(this Func<ResultToCache> original)
         {
-            return original;
+            var is_cached = false;
+            ResultToCache cached_value = default(ResultToCache);
+
+            return () =>
+            {
+                if (is_cached) return cached_value;
+
+                is_cached = true;
+                return (cached_value =original());
+            };
         }
     }
 }

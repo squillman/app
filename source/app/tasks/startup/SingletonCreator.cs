9a17db2d0c1c85cc42470a3f7f1d5b9f9c73b6cd
git_name <git_email>
2011-10-21 15:50:17 -0400

    Pushing new changes

---
+++ source/app/tasks/startup/SingletonCreator.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using app.infrastructure.containers.simple;
+using app.tasks.startup;
+
+public class SingletonCreator : Dictionary<Type, ICreateASingleDependency>,
+                                                 IRegisterComponentsIntoTheContainer
+{
+    private ContainerRegistrationFacility original;
+    private ICreateDependencyFactories dependency_factories_factory;
+
+    public SingletonCreator(ICreateDependencyFactories dependency_factories_factory,ContainerRegistrationFacility original)
+    {
+        this.original = original;
+        this.dependency_factories_factory = dependency_factories_factory;
+    }
+
+    public void register_instance<Contract>(Contract instance)
+    {
+        original.register<Contract>();
+    }
+
+    public void register<Contract, Implementation>() where Implementation : Contract
+    {
+        original.register<Contract,Implementation>();
+    }
+
+    public void register<Contract>()
+    {
+        original.register<Contract>();
+    }
+}
